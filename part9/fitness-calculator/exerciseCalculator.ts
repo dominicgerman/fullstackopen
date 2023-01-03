27b2802f34961afd1813d2e67a74301dc99d116b
dominicgerman <dominicgerman@gmail.com>
2023-01-03 15:01:26 -0600

    9.7

--- part9/fitness-calculator/exerciseCalculator.ts
+++ part9/fitness-calculator/exerciseCalculator.ts
@@ -8,7 +8,7 @@ interface Result {
   average: number
 }
 
-const calculateExercises = (args: Array<number>): Result => {
+export const calculateExercises = (args: Array<number>): Result => {
   const periodLength = args.length - 1
   const trainingDays = args.slice(1).filter((num) => num > 0).length
   const totalHours = args
@@ -45,30 +45,30 @@ const calculateExercises = (args: Array<number>): Result => {
   }
 }
 
-const parseArgs = (args: Array<string>) => {
-  if (args.length < 4) throw new Error('Not enough arguments!')
-  if (args.length > 12) throw new Error('Too many arguments!')
-  if (isNaN(Number(args[2])))
-    throw new Error('Provided value for "target" is not a number!')
+// const parseArgs = (args: Array<string>) => {
+//   if (args.length < 4) throw new Error('Not enough arguments!')
+//   if (args.length > 12) throw new Error('Too many arguments!')
+//   if (isNaN(Number(args[2])))
+//     throw new Error('Provided value for "target" is not a number!')
 
-  let target: number = Number(args[2])
-  const values = args.slice(3).map((val) => Number(val))
-  values.forEach((num) => {
-    if (isNaN(num)) {
-      throw new Error('Provided values were not numbers')
-    }
-  })
-  return [target, ...values]
-}
+//   const target = Number(args[2])
+//   const values = args.slice(3).map((val) => Number(val))
+//   values.forEach((num) => {
+//     if (isNaN(num)) {
+//       throw new Error('Provided values were not numbers')
+//     }
+//   })
+//   return [target, ...values]
+// }
 
-try {
-  const parsedData: Array<number> = parseArgs(process.argv)
-  console.log(calculateExercises(parsedData))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
-  }
-  console.log(errorMessage)
-}
+// try {
+//   const parsedData: Array<number> = parseArgs(process.argv)
+//   console.log(calculateExercises(parsedData))
+// } catch (error: unknown) {
+//   let errorMessage = 'Something bad happened.'
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message
+//   }
+//   console.log(errorMessage)
+// }
 // console.log(calculateExercises([0, 0, 2, 1, 0, 0, 1]))
--- part9/fitness-calculator/index.ts
+++ part9/fitness-calculator/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { calculateBmi } from './bmiCalculator'
+import { calculateExercises } from './exerciseCalculator'
 const app = express()
 
-app.get('/hello', (req, res) => {
-  console.log(req.query)
+app.use(express.json())
+
+app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
 })
 
@@ -21,6 +23,39 @@ app.get('/bmi', (req, res) => {
     height: Number(height),
     bmi: result,
   })
+})
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body
+  console.log(daily_exercises, target)
+
+  const missingParams: boolean = !daily_exercises || !target
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const invalidParams: boolean =
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    isNaN(target) ||
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    daily_exercises.forEach((num: number) => {
+      isNaN(num)
+    })
+
+  if (invalidParams) {
+    res.status(400).json({
+      error: 'malformatted parameter(s)',
+    })
+  }
+
+  if (missingParams) {
+    res.status(400).json({
+      error: 'parameter(s) missing',
+    })
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment
+  const result = calculateExercises([target, ...daily_exercises])
+
+  res.json(result)
 })
 
 const PORT = 3003
